fcae40272f7818594ba627fe31d33506db9fd78d
Matt Whalley <matt@mattwhalley.com>
2019-05-22 09:32:37 -0400

    updated contrast script for bc-net; added  folder & renamed  folder to

--- b-org/functions/employeeDiscounts.js
+++ b-org/functions/employeeDiscounts.js
@@ -42,10 +42,11 @@ $(document).ready(function () {
           '</section>';
 
         // var of star counts from GET
-        //console.log(items[j].AvgRating)
+        stars = $(this).parent().children('li.star');
+        console.log(stars)
         for (i = 0; i < items[j].AvgRating; i++) {
           //console.log(this);
-          $('#stars[data-id="' + items[j].ID + '"] > .star').addClass('selected');
+          $(stars[i]).addClass('selected');
         }
 
         console.log(items[j].ID);
@@ -174,7 +175,36 @@ $(document).ready(function () {
                     //ratingPost();
                   }
                 });
+        /*
+                $.ajax({
+                  url: "http://bc-net/_api/web/lists/GetByTitle('specialDiscounts')/items",
+                  method: 'GET',
+                  headers: {
+                    'Accept': 'application/json; odata=verbose'
+                  },
+                  success: function (data) {
+                    var items = data.d.results;
+                    // console.log(items);
+                    // onStar = var for rating selected by user
+                    // TODO: var for item ID for use in POST url
+                    var newRating = onStar; // var for rating selected by user to go here
+                    // console.log(newRating);
+                    var avgRating = Number(items[0].AvgRating); // GET for this calculation only
+                    // console.log(avgRating);
+                    var ratingCount = items[0].RatingCt; // GET for this calculation only
+                    // console.log(ratingCount);
+                    newCount = ratingCount + 1; // POST this to ratingCt column
+                    // console.log(newCount);
+                    var numerator = avgRating * ratingCount + newRating; // use as local var for this calculation only
+                    // console.log(numerator);
+                    newAvg = Math.round(numerator / newCount); // POST to avgRating column
+                    console.log(newAvg);
+                    //refreshToken();
+                    //ratingPost();
+                  }
+                });
         
+        */
         */
         function refreshToken() {
           $.ajax({
